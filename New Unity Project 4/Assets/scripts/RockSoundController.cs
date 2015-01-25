287829320fac048b9569fbec127dc2c5a1e62714
Jeffrey Johnson <jefjohms@gmail.com>
2015-01-24 23:56:47 -0800

    :Ribbon: Yayness, First Commit

---
+++ New Unity Project 4/Assets/scripts/RockSoundController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RockSoundController : MonoBehaviour {
+
+	// Use this for initialization
+    //public AudioClip rockScrap;
+    public AudioSource source;
+    public bool isSliding;
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        if (rigidbody2D.velocity.x != 0)
+        {
+            //Debug.Log(rigidbody.velocity.x);
+            isSliding = true;
+        }
+        else
+        {
+            isSliding = false;
+        }
+
+        PlayAudio();
+
+	}
+    private void PlayAudio()
+    {
+
+        if(isSliding && !audio.isPlaying)
+        {
+            //audio.clip = rockScrap;
+            source.Play();
+        }
+        else
+        {
+          //  source.Stop();
+        }
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 20), rigidbody2D.velocity.x.ToString());
+    }
+}
---
+++ New Unity Project 4/Assets/scripts/girlUserControl.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using UnitySampleAssets.CrossPlatformInput;
+
+namespace UnitySampleAssets._2D
+{
+
+    [RequireComponent(typeof(PlatformerCharacter2D))]
+    public class girlUserControl : MonoBehaviour
+    {
+        private PlatformerCharacter2D character;
+        private bool jump;
+        public bool isActive = true;
+        private GameObject branchMantelPoint;
+
+
+        private void Awake()
+        {
+            character = GetComponent<PlatformerCharacter2D>();
+            //branchMantelPoint = GameObject.Find("branch/mantelPoint");
+        }
+
+        private void Update()
+        {
+            if (!jump)
+                // Read the jump input in Update so button presses aren't missed.
+                jump = CrossPlatformInputManager.GetButtonDown("Jump");
+            //if (isManteling)
+            //{
+            //    float distCovered = (Time.time - startTime) * speed;
+            //    float fracJourney = distCovered / journeyLength;
+            //    transform.position = Vector3.Lerp(startMarker.position, endMarker.position, fracJourney);
+            //    if (transform.position == endMarker.position)
+            //        isManteling = false;
+            //}
+        }
+
+        private void FixedUpdate()
+        {
+            if (isActive)
+            {
+                // Read the inputs.
+                bool crouch = Input.GetKey(KeyCode.LeftControl);
+                float h = CrossPlatformInputManager.GetAxis("Horizontal");
+                // Pass all parameters to the character control script.
+                character.Move(h, crouch, jump);
+                jump = false;
+
+            }
+
+        }
+
+        //test code for lerping up branch
+        //public Transform startMarker;
+        //public Transform endMarker;
+        //public float speed = 1.0f;
+        //private float startTime;
+        //private float journeyLength;
+        //public Transform target;
+        //public float smooth = 5.0f;
+        private bool isManteling;
+
+
+        void OnCollisionEnter2D(Collision2D collider)
+        {
+            //Debug.Log(collider.transform.ToString());
+
+            //if (collider.transform == branchMantelPoint.transform)
+            //{
+            //    //startTime = Time.time;
+            //    //startMarker = transform.Find("GroundCheck");
+            //    //endMarker = GameObject.Find("branch").transform;
+            //    //BoxCollider2D endMarkerBox = endMarker.GetComponent<BoxCollider2D>();
+            //    //journeyLength = Vector3.Distance(startMarker.position, endMarker.position + endMarkerBox.bounds.extents);
+            //    Renderer rend = GetComponent<Renderer>();
+            //    transform.position = new Vector3(transform.position.x + rend.bounds.extents.x,
+            //       rend.bounds.extents.y * .5f,
+            //        transform.position.z);
+
+
+            //    ////playerBottomPos = branch.position;
+            //    //Vector3 sub = branchTopPos - playerBottomPos;
+            //    //transform.Translate(sub.x, sub.y, sub.z, null);
+            //    isManteling = true;
+            //}
+
+        }
+
+        public void SwapPlayer()
+        {
+            isActive = !isActive;
+        }
+    }
+}
